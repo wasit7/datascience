669017f6fba9877f1e6cc138fcc0b613b17c5471
SothanaV <nai6320@gmail.com>
2019-05-08 18:07:53 +0700

    edit dr.wasit bug

--- extra/main.py
+++ extra/main.py
@@ -8,13 +8,13 @@ async def index(request):
     with open('index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
 
-@sio.on('message')
+@sio.on('channel')
 async def print_message(sid, message):
     print("Socket ID: " , sid)
-    print(message)
+    print(type(message),message)
     # await a successful emit of our reversed message
     # back to the client
-    await sio.emit('message', message[::-1])
+    await sio.emit('message', message)
 
 app.router.add_get('/', index)
 
